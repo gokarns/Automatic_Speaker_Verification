6a7ffa0e8d603e76d53357de4e361f7e0ccc5a0f
Noel Jensen <46387451+n-jensen@users.noreply.github.com>
2021-06-04 14:54:22 -0400

    add

--- live_audio.py
+++ live_audio.py
@@ -1,11 +1,25 @@
-from playsound import playsound
-import numpy as np
-import simpleaudio as sa
+####python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+
+import sounddevice as sd
 from scipy.io.wavfile import write
+# import simpleaudio as sa
+from playsound import playsound
 
-fs = 44100  # Sample rate
-seconds = 3  # Duration of recording
 
-myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-sd.wait()  # Wait until recording is finished
-write('output.wav', fs, myrecording)  # Save as WAV file 
+def record(filename):    
+    fs = 44100  # Sample rate
+    seconds = 3  # Duration of recording
+
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    sd.wait()  # Wait until recording is finished
+    write(filename, fs, myrecording)  # Save as WAV file 
+
+
+def play(filename):
+    # wave_obj = sa.WaveObject.from_wave_file(filename)
+    # play_obj = wave_obj.play()
+    # play_obj.wait_done()
+    playsound(filename)
+
+record('output.wav')
+play('output.wav')
